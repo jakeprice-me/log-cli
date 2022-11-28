80a4a92983d517a8e465809f2e108a3a8fb1e3c9
Jake Price <git@jakeprice.dev>
2022-11-28 12:10:47 +0000

    Rename main.py to avoid clash with other scripts

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='log-cli',
     version='0.2.2',
-    py_modules=['main'],
+    py_modules=['log_cli'],
     install_requires=[
         'Click',
     ],
     entry_points={
         'console_scripts': [
-            'log-cli = main:cli',
+            'log-cli = log_cli:cli',
         ],
     },
 )
